3c0a7d620b72436748b026335ee08215656c5164
Mykola Makhin <Mykola.Makhin@gmail.com>
2017-02-21 23:37:52 +0200

    Added auth token authentication support

--- src/main/java/x/mvmn/gp2liveviewsrv/GP2LiveViewSrv.java
+++ src/main/java/x/mvmn/gp2liveviewsrv/GP2LiveViewSrv.java
@@ -1,9 +1,23 @@
 package x.mvmn.gp2liveviewsrv;
 
+import java.io.IOException;
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -14,6 +28,7 @@ public class GP2LiveViewSrv {
 		final Options cliOptions = new Options();
 
 		cliOptions.addOption("port", true, "HTTP port.");
+		cliOptions.addOption("authToken", true, "Optional authToken - to be sent with .");
 
 		final CommandLine commandLine = new PosixParser().parse(cliOptions, args);
 		if (commandLine.hasOption("port")) {
@@ -30,18 +45,42 @@ public class GP2LiveViewSrv {
 			}
 		}
 
-		new GP2LiveViewSrv(port != null ? port.intValue() : 8080).start();
+		new GP2LiveViewSrv(port != null ? port.intValue() : 8080, commandLine.hasOption("authToken") ? commandLine.getOptionValue("authToken") : null).start();
 	}
 
 	protected final Server server;
 
-	public GP2LiveViewSrv(int port) {
+	public GP2LiveViewSrv(int port, final String authToken) {
 		this.server = new Server(port);
 		this.server.setStopAtShutdown(true);
 
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		server.setHandler(context);
 		context.addServlet(new ServletHolder(new GP2ApiServlet()), "/");
+		if (authToken != null) {
+			Filter tokenAuthFilter = new Filter() {
+
+				public void init(FilterConfig filterConfig) throws ServletException {
+				}
+
+				public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+					if (request instanceof HttpServletRequest) {
+						HttpServletRequest httpRequest = (HttpServletRequest) request;
+						if (authToken.equals(httpRequest.getHeader("X-AuthToken"))) {
+							chain.doFilter(httpRequest, response);
+						}
+					} else {
+						if (response instanceof HttpServletResponse) {
+							((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+						}
+					}
+				}
+
+				public void destroy() {
+				}
+			};
+			context.addFilter(new FilterHolder(tokenAuthFilter), "/*", EnumSet.of(DispatcherType.REQUEST));
+		}
 	}
 
 	public void start() {
